505d8b871789efe58f79dc7a1d26604696aa1cc6
Vishal Kumar <vishal808066@gmail.com>
2025-11-19 16:32:48 +0530

    Initial commit

--- lib/utils/access_control.dart
+++ lib/utils/access_control.dart
@@ -4,6 +4,11 @@ final Map<String, List<String>> userAccess = {
     "ble_dev_kit.jpg","stts751.png","memory.png","bleNode.png","programmer.png","tlv.png","bme680.png",
     "Actitvity.png","vl5.png","ltr390.png","halleffect.png","ir_sensor.png",
   ],
+    "Pulkit":  [
+    "dataloggerrender.png","blegateway.png","groove.png","lux.png","lis3dh.png","buzzer.png","relay.png",
+    "ble_dev_kit.jpg","stts751.png","memory.png","bleNode.png","programmer.png","tlv.png","bme680.png",
+    "Actitvity.png","vl5.png","ltr390.png","halleffect.png","ir_sensor.png",
+  ],
   "suman": ["lux.png", "lis3dh.png", "buzzer.png", "relay.png"],
   "harsh": ["memory.png", "bleNode.png", "ble_dev_kit.jpg"],
   "sejal": ["programmer.png", "tlv.png"],
