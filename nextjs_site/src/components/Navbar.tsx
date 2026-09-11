c73ec2bc0df10d55867825df638fd36c72ae58a7
sksuman14 <sksuman14@gmail.com>
2026-09-11 11:37:00 +0530

    feat: replace logo with transparent PNG version

--- nextjs_site/src/components/Navbar.tsx
+++ nextjs_site/src/components/Navbar.tsx
@@ -146,7 +146,7 @@ export default function Navbar() {
           <Link href="/home" className="group flex items-center gap-3">
             <div className="relative w-32 h-12 transition-transform duration-300 group-hover:scale-105">
               <Image
-                src="/images/logo.jpg"
+                src="/images/logo.png"
                 alt="Logo"
                 fill
                 className="object-contain"
